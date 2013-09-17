f097bd0b67689e62132bb3cda53f1d8f559454d8
zltqzj <zltqzj@163.com>
2013-09-17 16:24:18 +0800

    objc_setAssociatedObject
    字符串与数组关联

--- objc_setAssociatedObject/main.m
+++ objc_setAssociatedObject/main.m
@@ -7,7 +7,7 @@
 //
 
 #import <Foundation/Foundation.h>
-
+#import <objc/runtime.h>
 int main(int argc, const char * argv[])
 {
 
@@ -15,7 +15,18 @@ int main(int argc, const char * argv[])
         
         // insert code here...
         NSLog(@"Hello, World!");
+        static char overviewKey;
+        NSArray *array =[[NSArray alloc] initWithObjects:@"One", @"Two", @"Three", nil];
+        //为了演示的目的，这里使用initWithFormat:来确保字符串可以被销毁
+        NSString * overview = [[NSString alloc] initWithFormat:@"%@",@"First three numbers"];
+        objc_setAssociatedObject(array, &overviewKey, overview, OBJC_ASSOCIATION_RETAIN);
+        [overview release];
         
+        NSString *associatedObject = (NSString *)objc_getAssociatedObject(array, &overviewKey);
+        NSLog(@"associatedObject:%@", associatedObject);
+        
+        objc_setAssociatedObject(array, &overviewKey, nil, OBJC_ASSOCIATION_ASSIGN);
+        [array release];
     }
     return 0;
 }
